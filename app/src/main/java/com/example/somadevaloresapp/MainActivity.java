506dd54f63837d1245d7df932f090f4acb3595f4
Allysson Santos <68858220+Allyssonantos@users.noreply.github.com>
2022-10-31 22:46:53 -0300

    01

--- app/src/main/java/com/example/somadevaloresapp/MainActivity.java
+++ app/src/main/java/com/example/somadevaloresapp/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void somarValores(View view) {
-        
+        // Recupera componentes
+        EditText edValor1 = (EditText) findViewById(R.id.valor1);
+        EditText edValor2 = (EditText) findViewById(R.id.valor2);
+
+        // Recupera valores e faz a soma
+        double valor1 = Double.parseDouble(edValor1.getText().toString());
+        double valor2 = Double.parseDouble(edValor1.getText().toString());
+        double resultado = valor1 + valor2;
+
+        // Guarda resultado
+        TextView txResultado = (TextView) findViewById(R.id.resultado);
+        txResultado.setText("O valor da soma = " + resultado );
+
     }
 }
